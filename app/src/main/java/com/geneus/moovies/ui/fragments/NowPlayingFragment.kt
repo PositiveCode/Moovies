9fbb0e8a8ad0baa38c42598960c90df32fafe027
eugene <seriously.eugene@gmail.com>
2022-05-15 23:03:45 +0800

    [/] added resource handling logic for api responses.

--- app/src/main/java/com/geneus/moovies/ui/fragments/NowPlayingFragment.kt
+++ app/src/main/java/com/geneus/moovies/ui/fragments/NowPlayingFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.geneus.moovies.R
 import com.geneus.moovies.ui.viewmodel.NowPlayingViewModel
+import com.geneus.moovies.utils.Status
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NowPlayingFragment : Fragment() {
@@ -22,5 +24,36 @@ class NowPlayingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         vm.getNowPlayingMovies()
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        vm.movieList.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    it.data?.let { moviesList ->
+                        Toast.makeText(
+                            context,
+                            "SUCCESS",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                Status.LOADING -> {
+                    Toast.makeText(
+                        context,
+                        "LOADING",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                Status.ERROR -> {
+                    Toast.makeText(
+                        context,
+                        "${it.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/geneus/moovies/ui/viewmodel/NowPlayingViewModel.kt
+++ app/src/main/java/com/geneus/moovies/ui/viewmodel/NowPlayingViewModel.kt
@@ -1,17 +1,27 @@
 package com.geneus.moovies.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.geneus.moovies.data.api.model.Movie
 import com.geneus.moovies.data.repo.MovieRepo
+import com.geneus.moovies.utils.Resource
 import kotlinx.coroutines.launch
 
 class NowPlayingViewModel(
     private val repo: MovieRepo
 ) : ViewModel() {
 
+    private val _movieList =
+        MutableLiveData<Resource<List<Movie>>>()
+    val movieList: LiveData<Resource<List<Movie>>>
+        get() = _movieList
+
     fun getNowPlayingMovies() {
         viewModelScope.launch {
-            repo.getMoviesByCategory(MovieRepo.Category.NOW_PLAYING)
+            _movieList.postValue(Resource.loading(null))
+            _movieList.postValue(repo.getMoviesByCategory(MovieRepo.Category.NOW_PLAYING))
         }
     }
 }
